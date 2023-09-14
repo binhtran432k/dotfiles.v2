f2cf3a10e47ba10604703c3f0a29482bf0ac3523
Binh Duc Tran <binhtran432k@gmail.com>
2023-09-14 15:59:40 +0700

    fix(nvim): not recognize general lsps

--- nvim/lua/user/plugins/lsp/general.lua
+++ nvim/lua/user/plugins/lsp/general.lua
@@ -1,18 +1,20 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    servers = {
-      bashls = {},
-      clangd = {},
-      cssls = {},
-      docker_compose_language_service = {},
-      dockerls = {},
-      gradle_ls = {},
-      hls = {},
-      html = {},
-      kotlin_language_server = {},
-      lemminx = {},
-      taplo = {},
+    opts = {
+      servers = {
+        bashls = {},
+        clangd = {},
+        cssls = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        gradle_ls = {},
+        hls = {},
+        html = {},
+        kotlin_language_server = {},
+        lemminx = {},
+        taplo = {},
+      },
     },
   },
 }
