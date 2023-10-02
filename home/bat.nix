7346445d3bb29219419d53de8b4b96d802cb5b64
Binh Duc Tran <binhtran432k@gmail.com>
2023-10-02 11:26:05 +0700

    update bat config

--- home/bat.nix
+++ home/bat.nix
@@ -8,5 +8,13 @@
       theme = user.colorscheme.bat;
       map-syntax = [ "*.jenkinsfile:Groovy" "*.props:Java Properties" ];
     };
+    extraPackages = with pkgs.bat-extras; [
+      batgrep
+      batman
+      batpipe
+      batwatch
+      batdiff
+      prettybat
+    ];
   };
 }
