01d092ccc4576270798de0b2abb802a20e307c5e
Binh Duc Tran <binhtran432k@gmail.com>
2023-11-20 20:38:13 +0700

    feat(wezterm): add harfbuzz

--- home/wezterm/config/init.lua
+++ home/wezterm/config/init.lua
@@ -12,28 +12,57 @@ end
 
 -- This is where you actually apply your config choices
 -- config.font = wezterm.font({ family = "Cascadia Code", harfbuzz_features = { "calt=1", "ss01=1" } })
-config.font = wezterm.font({ family = "Monaspace Neon" })
+local harfbuzz = {
+	"calt=1",
+	"liga=1",
+	"dlig=1",
+	-- "ss01=1",
+	-- "ss01=1",
+	-- "ss02=1",
+	-- "ss03=1",
+	-- "ss04=1",
+	-- "ss05=1",
+	-- "ss06=1",
+	-- "ss07=1",
+	-- "ss08=1",
+}
+config.font = wezterm.font({ family = "Monaspace Neon", harfbuzz_features = harfbuzz })
 config.bold_brightens_ansi_colors = true
 config.font_rules = {
 	{
 		italic = false,
 		intensity = "Bold",
-		font = wezterm.font({ family = "Monaspace Krypton", weight = "Bold" }),
+		font = wezterm.font({ family = "Monaspace Krypton", weight = "Bold", harfbuzz_features = harfbuzz }),
+	},
+	{
+		italic = false,
+		intensity = "Half",
+		font = wezterm.font({ family = "Monaspace Krypton", weight = "DemiBold", harfbuzz_features = harfbuzz }),
 	},
 	{
 		italic = true,
 		intensity = "Bold",
-		font = wezterm.font({ family = "Monaspace Radon", weight = "Bold", style = "Italic" }),
+		font = wezterm.font({
+			family = "Monaspace Radon",
+			weight = "Bold",
+			style = "Italic",
+			harfbuzz_features = harfbuzz,
+		}),
 	},
 	{
 		italic = true,
 		intensity = "Half",
-		font = wezterm.font({ family = "Monaspace Radon", weight = "DemiBold", style = "Italic" }),
+		font = wezterm.font({
+			family = "Monaspace Radon",
+			weight = "DemiBold",
+			style = "Italic",
+			harfbuzz_features = harfbuzz,
+		}),
 	},
 	{
 		italic = true,
 		intensity = "Normal",
-		font = wezterm.font({ family = "Monaspace Radon", style = "Italic" }),
+		font = wezterm.font({ family = "Monaspace Radon", style = "Italic", harfbuzz_features = harfbuzz }),
 	},
 }
 
