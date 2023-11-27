5f53dc903270bf79617305d8d3521af5d112abb2
Binh Duc Tran <binhtran432k@gmail.com>
2023-11-27 19:24:19 +0700

    feat(yazi): make yazi sort by default

--- home/yazi.nix
+++ home/yazi.nix
@@ -1,18 +1,22 @@
 { ... }:
-let
-  yazi-simple = ''
-    [manager]
-    syntect_theme = "~/.config/bat/themes/custom.tmTheme"
-    [status]
-    separator_open  = ""
-    separator_close = ""
-  '';
-in
 {
-  home.file.".config/yazi/theme.toml".text = yazi-simple;
-
   programs.yazi = {
     enable = true;
     enableFishIntegration = true;
+    theme = {
+      manager = {
+        syntect_theme = "~/.config/bat/themes/custom.tmTheme";
+      };
+      status = {
+        separator_open = "";
+        separator_close = "";
+      };
+    };
+    settings = {
+      manager = {
+        sort_by = "alphabetical";
+        sort_reverse = false;
+      };
+    };
   };
 }
