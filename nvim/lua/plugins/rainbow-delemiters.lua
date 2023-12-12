3e82ab1a1d784c09adbb12f639f5bec5641c40bb
Binh Duc Tran <binhtran432k@gmail.com>
2023-12-12 20:32:17 +0700

    refactor(nvim/rainbow): use default strategy

--- nvim/lua/plugins/rainbow-delemiters.lua
+++ nvim/lua/plugins/rainbow-delemiters.lua
@@ -1,28 +1,11 @@
---- Get rainbow delimiters strategy base on errors
----@return table?
-local function get_strategy()
-  local errors = 200
-  for _, tree in ipairs(vim.treesitter.get_parser():trees()) do
-    if tree:root():has_error() then
-      errors = errors - 1
-    elseif errors <= 0 then
-      return nil
-    end
-  end
-  return require("rainbow-delimiters").strategy.global
-end
-
 return {
   "HiPhish/rainbow-delimiters.nvim",
   event = { "LazyFile" },
   config = function()
     vim.g.rainbow_delimiters = {
-      strategy = {
-        [""] = get_strategy,
-      },
       query = {
-        [""] = "rainbow-delimiters",
-        ["javascript"] = "rainbow-delimiters-react",
+        javascript = "rainbow-delimiters-react",
+        query = "rainbow-blocks",
       },
       highlight = {
         "RainbowDelimiterRed",
