b8993c4ade3514e0ed5387f31fe0cacd5a576222
Binh Duc Tran <binhtran432k@gmail.com>
2023-12-12 19:34:24 +0700

    refactor(nvim/comment): use other lib

--- nvim/lua/plugins/default.lua
+++ nvim/lua/plugins/default.lua
@@ -1,6 +1,7 @@
 return {
   { "folke/tokyonight.nvim" },
   { "catppuccin/nvim", name = "catppuccin", enabled = false },
+  { "echasnovski/mini.comment", enabled = false },
   { import = "plugins.lang" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 }
