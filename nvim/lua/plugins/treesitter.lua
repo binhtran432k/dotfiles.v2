496867044e8ffaa67f4307e5fe5987cf76c21693
Binh Duc Tran <binhtran432k@gmail.com>
2023-12-12 19:35:10 +0700

    refactor(treesitter): separate config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,6 @@
 return {
-  {
-    "mizlan/iswap.nvim",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    cmd = {
-      "ISwapNodeWith",
-      "ISwapNode",
-      "ISwapNodeWithRight",
-      "ISwapNodeWithLeft",
-    },
-    config = true,
-  },
-
-  {
-    "danymat/neogen",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    keys = {
-      { "<leader>cc", "<cmd>Neogen<cr>", desc = "Neogen document" },
-    },
-    cmd = "Neogen",
-    opts = {
-      -- input_after_comment = false,
-      enable_placeholders = false,
-      snippet_engine = nil, -- "luasnip"
-    },
-  },
-
-  {
-    "windwp/nvim-ts-autotag",
-    opts = {
-      enable_close_on_slash = false,
-    },
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    indent = { enable = false },
   },
 }
