69fd84ee65b5114152ce19e7251803eeed5d027f
Binh Duc Tran <binhtran432k@gmail.com>
2023-12-13 11:48:33 +0700

    feat: use RTC time

--- hosts/kouta/default.nix
+++ hosts/kouta/default.nix
@@ -11,6 +11,9 @@
 
   nixpkgs.overlays = import ../../overlays args;
 
+  # Use RTC time
+  time.hardwareClockInLocalTime = true;
+
   # Enable binfmt emulation of aarch64-linux, this is required for cross compilation.
   boot.binfmt.emulatedSystems = [ "aarch64-linux" "riscv64-linux" ];
   # supported fil systems, so we can mount any removable disks with these filesystems
